849d563f98d1038718efc7050190a4300691e496
ravenvst <142888381+ravenvst@users.noreply.github.com>
2024-01-31 11:23:45 +0100

    add operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let x = 0;
+let y = 0;
+let operator = "";
+
 function add (x, y) {
     return (x + y);
 }
@@ -12,4 +16,22 @@ function multiply (x, y) {
 
 function divide (x, y) {
     return (x / y);
+}
+
+function operate (operator, number1, number2) {
+    switch(operator) {
+        case "-":
+            return subtract(number1, number2);
+            break;
+        case "+":
+            return add(number1, number2);
+            break;
+        case "*":
+            return multiply(number1, number2);
+            break;
+        case "/":
+            return divide(number1, number2);
+            break;
+    }
+    
 }
