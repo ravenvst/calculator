fe86f4632373b5e4fa403e68eaedf2fccea04178
ravenvst <142888381+ravenvst@users.noreply.github.com>
2024-02-12 17:31:44 +0100

    writes out pressed button values

--- javascript.js
+++ javascript.js
@@ -1,6 +1,30 @@
 let x = 0;
 let y = 0;
 let operator = "";
+let displayValue = 0;
+
+let display = document.getElementById("display")
+display.innerHTML = displayValue;
+
+function changeDisplayValue(btnText){
+    if (display.innerHTML == 0) {
+        display.innerHTML = btnText;
+    } else {
+        display.innerHTML += btnText;
+        displayValue = btnText;
+    }
+}
+
+let calcButtons = document.querySelectorAll(".calc-button");
+
+for (let i = 0; i < calcButtons.length; i++) {
+    calcButtons[i].addEventListener('click', function () {
+        changeDisplayValue(calcButtons[i].innerHTML);
+    }, false);
+}
+
+
+
 
 function add (x, y) {
     return (x + y);
